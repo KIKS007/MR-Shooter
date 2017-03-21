086264906579bb03b0df41a99ac20e91c0e00c4a
Keryann Malacchina <keryannm@gmail.com>
2017-03-21 11:05:00 +0100

    Babel and AI

---
+++ Assets/SCRIPTS/EnemyManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyManager : MonoBehaviour 
+{
+	[Header ("Enemies")]
+	public Transform enemiesParent;
+	public GameObject[] enemiesPrefabs = new GameObject[0];
+	public Transform[] enemiesSpawns = new Transform[0];
+	public Transform enemiesTarget;
+
+	[Header ("Infos")]
+	public int enemiesCount = 0;
+
+	[Header ("Settings")]
+	public int maxEnemiesCount = 0;
+	public float addUpDuration = 5f;
+	public Vector2 randomDurationBetweenSpawns;
+	public Vector2 enemiesSpeed;
+	public float speedMin;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (Spawner ());
+		StartCoroutine (AddUp ());
+	}
+
+	IEnumerator AddUp ()
+	{
+		maxEnemiesCount++;
+
+		yield return new WaitForSeconds (addUpDuration);
+
+		StartCoroutine (AddUp ());
+	}
+
+	IEnumerator Spawner ()
+	{
+		if (enemiesCount < maxEnemiesCount)
+			SpawnEnemy ();
+
+		yield return new WaitWhile (()=> enemiesCount == maxEnemiesCount);
+
+		yield return new WaitForSeconds (Random.Range (randomDurationBetweenSpawns.x, randomDurationBetweenSpawns.y));
+
+		StartCoroutine (Spawner ());
+	}
+
+	void SpawnEnemy ()
+	{
+		enemiesCount++;
+
+		GameObject agent = Instantiate (enemiesPrefabs [Random.Range (0, enemiesPrefabs.Length)], enemiesSpawns [Random.Range (0, enemiesSpawns.Length)].position, Quaternion.identity, enemiesParent) as GameObject;
+
+		StartCoroutine (WaitTillAgentEnabled (agent));
+	}
+
+	IEnumerator WaitTillAgentEnabled (GameObject agent)
+	{
+		yield return new WaitUntil (()=> agent.activeSelf == true);
+
+		agent.GetComponent<NavMeshAgent> ().SetDestination (enemiesTarget.position);
+		agent.GetComponent<NavMeshAgent> ().speed = speedMin + maxEnemiesCount + Random.Range (enemiesSpeed.x, enemiesSpeed.y);
+	}
+
+	public void EnemyDead ()
+	{
+		enemiesCount--;
+	}
+}
---
+++ Assets/SCRIPTS/ImageTargetSize.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetSize : MonoBehaviour 
+{
+	public float height = 1;
+	private ImageTargetBehaviour _imageTarget;
+
+	// Use this for initialization
+	void Start () {
+		_imageTarget = GetComponent<ImageTargetBehaviour> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_imageTarget.SetHeight (height);
+	}
+}
